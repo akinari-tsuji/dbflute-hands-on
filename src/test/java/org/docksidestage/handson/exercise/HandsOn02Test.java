94b3baf57a636431f49738d74e102f95fab8a3a5
akinari.tsuji <akinari.tsuji@bizreach.co.jp>
2026-06-10 14:57:03 +0900

    ちょっと続き

--- src/test/java/org/docksidestage/handson/exercise/HandsOn02Test.java
+++ src/test/java/org/docksidestage/handson/exercise/HandsOn02Test.java
@@ -1,6 +1,32 @@
 package org.docksidestage.handson.exercise;
 
+import javax.annotation.Resource;
+
+import org.dbflute.bhv.readable.CBCall;
+import org.docksidestage.handson.dbflute.cbean.MemberCB;
+import org.docksidestage.handson.dbflute.exbhv.MemberBhv;
 import org.docksidestage.handson.unit.UnitContainerTestCase;
 
 public class HandsOn02Test extends UnitContainerTestCase {
+    @Resource
+    private MemberBhv memberBhv;
+
+    public void test_existsTestData() throws Exception {
+        // ## Arrange ##
+
+        // ## Act ##
+        int count = memberBhv.selectCount(cb -> {});
+
+        // ## Assert ##
+        assertTrue(count > 0);
+    }
+    
+    public void test_memberNameStartsWithS() throws Exception {
+        // ## Arrange ##
+        
+        // ## Act ##
+
+    
+        // ## Assert ##
+    }
 }
